8988949f67ba5545a52b235c5af606b3449cbb03
thejuanitaemeh <141019518+thejuanitaemeh@users.noreply.github.com>
2023-09-05 15:05:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,68 @@
-//code to create web server
-const express = require('express');
-const router = express.Router();
-const Comment = require('../models/comment');
-const Post = require('../models/post');
-const commentsController = require('../controllers/comments_controller');
-const passport = require('passport');
+// Create web server using express
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+
+const comments = [
+  {
+    id: uuidv4(),
+    username: "todd",
+    comment: "lol that is so funny!",
+    post: "1",
+  },
+  {
+    id: uuidv4(),
+    username: "sk8erBoi",
+    comment: "plz delete your account, todd",
+    post: "1",
+  },
+  {
+    id: uuidv4(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+    post: "2",
+  },
+  {
+    id: uuidv4(),
+    username: "colt",
+    comment: "javascript is sooooo cooooollllll",
+    post: "2",
+  },
+];
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// GET /comments
+app.get("/comments", (req, res) => {
+  res.send(comments);
+});
+
+// POST /comments
+app.post("/comments", (req, res) => {
+  const { username, comment, post } = req.body;
+  comments.push({ username, comment, post, id: uuidv4() });
+  res.send("Comment added!");
+});
+
+// GET /comments/:id
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.send(comment);
+});
+
+// DELETE /comments/:id
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments.filter((c) => c.id !== id);
+  res.send("Comment deleted!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
 
